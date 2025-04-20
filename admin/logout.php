f638ea21e59d869a097c1ff838b4cb2043b4556c
XpoZed101 <butiongkarl@gmail.com>
2025-04-20 23:59:32 +0800

    try commit

--- admin/logout.php
+++ admin/logout.php
@@ -1,7 +1,8 @@
 <?php
-    session_unset($_SESSION['sess_adminid']);
-    session_unset($_SESSION['sess_adminemail']);
-    session_unset($_SESSION['admin_loggedin']);
+    session_start(); // Start the session
+    unset($_SESSION['sess_adminid']);
+    unset($_SESSION['sess_adminemail']);
+    unset($_SESSION['admin_loggedin']);
     // session_destroy();
     header("Location: login.php");
 ?>
